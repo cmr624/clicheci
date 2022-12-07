476ddfd900b5e1eabb8bcf256c547d850c93d544
Carlos Michael <carlosmichaeldev@gmail.com>
2022-12-07 16:21:06 -0500

    added fish dodging

--- Assets/Scripts/Minigames/FIsh/FishBezierNodeMove.cs
+++ Assets/Scripts/Minigames/FIsh/FishBezierNodeMove.cs
@@ -7,8 +7,8 @@ public class FishBezierNodeMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        LeanTween.moveLocalX(gameObject, transform.position.x-1f, 2f)
-            .setLoopPingPong()
-            .setEaseInCubic();
+        //Leantween.movelocalx(gameobject, transform.position.x-1f, 2f)
+        //    .setlooppingpong()
+        //    .seteaseincubic();
     }
 }
